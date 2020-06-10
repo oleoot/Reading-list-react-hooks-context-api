4f373a443adedfa48bd48e4b69c8b00513e91fac
oleoot <oleoot28@gmail.com>
2020-06-10 17:16:56 +0300

    Two components added + basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Navbar from './components/Navbar'
+import Booklist from './components/Booklist';
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Booklist />
     </div>
   );
 }
