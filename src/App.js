c74898b427877d4d60afc1934b3443f267ddf0ce
oleoot <oleoot28@gmail.com>
2020-06-11 09:55:45 +0300

    Toggling theme button, changing context data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Booklist from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Navbar />
         <Booklist />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
